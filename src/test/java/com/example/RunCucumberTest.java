3795e158dab63a5322e9e447a8e24b346a7ca898
JA-22 <pcmas@hotmail.com>
2024-11-23 07:50:42 -0300

    Update ChromeDriver installation script to handle missing versions

--- src/test/java/com/example/RunCucumberTest.java
+++ src/test/java/com/example/RunCucumberTest.java
@@ -2,11 +2,11 @@ package com.example;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
-
+//
 @CucumberOptions(
     features = "src/test/resources/features/example.feature",
     glue = "com.example",
     plugin = {"pretty", "html:target/cucumber-reports/cucumber-test-result.html"}
     )
-public class RunCucumberTest extends AbstractTestNGCucumberTests {
+public class RunCucumberTest extends AbstractTestNGCucumberTests { 
 }
