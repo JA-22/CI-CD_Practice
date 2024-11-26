cd34ff3b362b2b13e73a88126692928c06a6e42d
JA-22 <pcmas@hotmail.com>
2024-11-26 14:46:38 -0300

    update StepDefinitions class 5

--- src/test/java/com/example/StepDefinitionsTest.java
+++ src/test/java/com/example/StepDefinitionsTest.java
@@ -2,14 +2,6 @@ package com.example;
 
 import io.cucumber.java.en.Given;
 
-/*public class StepDefinitions {
-
-    @Given("I open the Google homepage")
-    public void iOpenTheGoogleHomepage() {
-        Base.getDriver().get("https://www.google.com");
-    }
-}
-    */
     
     public class StepDefinitionsTest {
         private String baseUrl;
