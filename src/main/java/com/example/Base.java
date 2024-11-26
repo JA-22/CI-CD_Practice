cc98486b2380d4d410c260c7092f2818c473dbd9
JA-22 <pcmas@hotmail.com>
2024-11-26 16:12:48 -0300

    update StepDefinitions class 11

--- src/main/java/com/example/Base.java
+++ src/main/java/com/example/Base.java
@@ -1,23 +1,20 @@
 package com.example;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Base {
-    protected static WebDriver driver;
+    private static WebDriver driver;
 
     public static void initializeDriver() {
         if (driver == null) {
-            // Usa WebDriverManager para manejar el ChromeDriver
             WebDriverManager.chromedriver().setup();
-
             ChromeOptions options = new ChromeOptions();
-            options.addArguments("--headless"); // Modo headless para CI/CD
+            options.addArguments("--headless"); // Modo headless
             options.addArguments("--no-sandbox");
             options.addArguments("--disable-dev-shm-usage");
-
             driver = new ChromeDriver(options);
         }
     }
--- src/test/java/com/example/StepDefinitionsTest.java
+++ src/test/java/com/example/StepDefinitionsTest.java
@@ -1,23 +1,33 @@
 package com.example;
 
 import io.cucumber.java.en.Given;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
-    
-    public class StepDefinitionsTest {
-        private String baseUrl;
-        private String environment;
-    
-        public StepDefinitionsTest() {
-            // Recuperar las variables de entorno configuradas
-            this.baseUrl = System.getProperty("baseUrl", "https://www.google.com");
-            this.environment = System.getProperty("env", "QA");
-            System.out.println("Running tests in environment: " + environment);
-            System.out.println("Base URL is: " + baseUrl);
-        }
-    
-        @Given("I open the Google homepage")
-        public void iOpenTheGoogleHomepage() {
-            Base.getDriver().get(baseUrl);
-        }
+public class StepDefinitionsTest {
+    private String baseUrl;
+    private String environment;
+
+    @BeforeClass
+    public void setUp() {
+        // Inicializar el WebDriver
+        Base.initializeDriver();
+        this.baseUrl = System.getProperty("baseUrl", "https://www.google.com");
+        this.environment = System.getProperty("env", "QA");
+        System.out.println("Running tests in environment: " + environment);
+        System.out.println("Base URL is: " + baseUrl);
     }
-    
+
+    @Test
+    @Given("I open the Google homepage")
+    public void iOpenTheGoogleHomepage() {
+        Base.getDriver().get(baseUrl);
+    }
+
+    @AfterClass
+    public void tearDown() {
+        // Cerrar el WebDriver
+        Base.quitDriver();
+    }
+}
